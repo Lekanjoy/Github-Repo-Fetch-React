273bcc62e1f09eb62b5f663bbb12576ebf696ddf
Alabi Olalekan Emmanuel <alabilekanemmanuel@gmail.com>
2022-11-03 19:53:20 +0100

    styled pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import prev from '../assets/3994376_arrow_back_left_navigation_previous_icon.svg'
+import next from '../assets/3994400_arrow_forward_navigation_next_right_icon.svg'
 
 const Pagination = ({
   reposPerPage,
@@ -24,30 +26,31 @@ const Pagination = ({
   }
   // console.log(pageNumber);
   return (
-    <div className="pagination mt-4 w-full">
+    <div className="pagination mt-4 w-full ">
       <p className="">Page {currentPage} of {pageNumber.length}</p> <br />
-      <button
-        className="bg-red-400 btn"
-        disabled={currentPage === 1}
-        onClick={prevPage}
-      >
-        Prev
-      </button>
-
-      {pageNumber.map((num) => {
-        return (
-          <button className="btn" key={num} onClick={() => paginate(num)}>
-            {num}
-          </button>
-        );
-      })}
-      <button
-        className="bg-green-600 btn"
-        disabled={currentPage === pageNumber.length}
-        onClick={nextPage}
-      >
-        Next
-      </button>
+      <div className="lg:flex lg:items-center lg:justify-center">
+        <button
+          className="btn flex items-center"
+          disabled={currentPage === 1}
+          onClick={prevPage}
+        >
+          <img src={prev} alt="" />
+        </button>
+        {pageNumber.map((num) => {
+          return (
+            <button  className={currentPage === num ? `btn bg-blue-500 text-white`: 'btn'} key={num} onClick={() => paginate(num)}>
+              {num}
+            </button>
+          );
+        })}
+        <button
+          className="btn flex items-center"
+          disabled={currentPage === pageNumber.length}
+          onClick={nextPage}
+        >
+          <img src={next} alt="" />
+        </button>
+      </div>
     </div>
   );
 };
--- src/components/pages/RepoData.jsx
+++ src/components/pages/RepoData.jsx
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react'
 import { useParams} from "react-router-dom";
 import {RepoContext} from '../../App'
+import github  from '../../assets/github_icon.svg'
 
 
 const RepoData = () => {
@@ -13,20 +14,23 @@ const RepoData = () => {
 
 
   return (
-    <section className="p-4 w-full   flex justify-center items-center min-h-[90vh]">
-      <div className="w-full rounded-md shadow-lg bg-white flex justify-center items-center flex-col border border-red-500 px-4 py-8 max-w-lg">
-        <h1 className="font-bold tracking-widest mb-3 underline"> {repoDetails?.name}
+    <section className="p-4 w-full bg-white  flex justify-center items-center min-h-[90vh]">
+      <div className="w-full rounded-md border-t-2  shadow-xl bg-white flex justify-center items-center flex-col  px-4 py-8 max-w-lg">
+        <h1 className="font-bold text-xl tracking-widest mb-1 text-center text-blue-500 "> {repoDetails?.name}
         </h1>
-        <p>
+        <p className='text-center mb-5 text-gray-500 text-sm'>
           {repoDetails?.description}
         </p>
         <div className="flex gap-x-2">
           <p><span className='font-bold'>Forks: </span>{repoDetails?.forks_count}</p>
-          <p><span className='font-bold'>Stars: </span>{repoDetails?.stargazers_count}</p>
+          <p><span className='font-bold '>Stars: </span>{repoDetails?.stargazers_count}</p>
+          <p><span className='font-bold '>Branch: </span>{repoDetails?.default_branch}</p>
         </div>
-        <p><span className='font-bold'>Primary Language: </span>{repoDetails?.language}</p>
+        <p className=''><span className='font-bold'>Primary Language: </span>{repoDetails?.language}</p>
         <p><span className='font-bold'>Date Created: </span>{repoDetails?.created_at}</p>
-        <a href={repoDetails?.html_url} className="bg-green-600 text-white font-semibold mt-4 text-center rounded-md p-3 w-full">Go to Code</a>
+        <a href={repoDetails?.html_url} className="bg-green-600  flex items-center justify-center gap-x-2
+         text-white font-semibold mt-8 text-center rounded-md p-3 w-full hover:bg-green-500">
+          <img src={github} alt="Github icon" className='w-[20px] h-[20px]' /> View Code</a>
       </div>
     </section>
   );
